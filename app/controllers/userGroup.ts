c4d7c98776c9be8a09fabcb3e488fcb522f56547
Fernando Soso <fernandososo324@gmail.com>
2024-05-11 06:54:55 -0300

    dev(controllers/userGroup.ts): added updateRole and deleteUserFromGroup arrow functions

--- app/controllers/userGroup.ts
+++ app/controllers/userGroup.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { prisma } from "../prisma";
 
 import { UserGroupService } from "../services/userGroup";
@@ -71,6 +71,45 @@ export const addUserToGroup = async (req: Request, res: Response) => {
         }
     } catch (error) {
         console.error('Error adding user to group: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const updateRole = async (req: Request, res: Response) => {
+    try{
+        const requesterId = parseInt(req.body.requesterId);
+        const userId = parseInt(req.body.userId);
+        const groupId = parseInt(req.body.groupId);
+        const role = req.body.role;
+
+        const userGroup = await userGroupService.updateRole(requesterId, userId, groupId, role);
+        
+        if (userGroup){
+            res.json(userGroup);
+        } else{
+            res.status(400).json({message: 'Could not update role'});
+        }
+    } catch (error) {
+        console.error('Error updating role: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const deleteUserFromGroup = async (req: Request, res: Response) => {
+    try{
+        const requesterId = parseInt(req.body.requesterId);
+        const userId = parseInt(req.body.userId);
+        const groupId = parseInt(req.body.groupId);
+
+        const deleted = await userGroupService.deleteUserFromGroup(requesterId, userId, groupId);
+        
+        if (deleted){
+            res.json({message: 'User deleted from group'});
+        } else{
+            res.status(400).json({message: 'Could not delete user from group'});
+        }
+    } catch (error) {
+        console.error('Error deleting user from group: ', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 }
