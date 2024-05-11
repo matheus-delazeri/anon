db7f5375954bf5b7c41912d14cf9dcfd81927ab8
Fernando Soso <fernandososo324@gmail.com>
2024-05-11 06:52:38 -0300

    dev(controllers/userGroup.ts): added getUsersFromGroup, addUserToGroup and getGroupsOfUser arrow functions

--- app/controllers/userGroup.ts
+++ app/controllers/userGroup.ts
@@ -23,5 +23,55 @@ export const getRole = async (req: Request, res: Response) => {
     }
 };
 
+export const getGroupsOfUser = async (req: Request, res: Response) => {
+    try {
+        const userId = parseInt(req.params.userId);
+        const groups = await userGroupService.getGroupsOfUser(userId);
 
+        if (groups){
+            res.json(groups);
+        }else{
+            res.status(404).json({message: 'User not found in any group'});
+        }
+    } catch (error) {
+        console.error('Error getting groups of user: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const getUsersFromGroup = async (req: Request, res: Response) => {
+    try {
+        const groupId = parseInt(req.params.groupId);
+        const users = await userGroupService.getUsersFromGroup(groupId);
+
+        if (users){
+            res.json(users);
+        }else{
+            res.status(404).json({message: 'No users found in this group'});
+        }
+    } catch (error) {
+        console.error('Error getting users from group: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const addUserToGroup = async (req: Request, res: Response) => {
+    try{
+        const requesterId = parseInt(req.body.requesterId);
+        const userId = parseInt(req.body.userId);
+        const groupId = parseInt(req.body.groupId);
+        const role = req.body.role;
+
+        const userGroup = await userGroupService.addUserToGroup(requesterId, userId, groupId, role);
+        
+        if (userGroup){
+            res.json(userGroup);
+        } else{
+            res.status(400).json({message: 'Could not add user to the group'});
+        }
+    } catch (error) {
+        console.error('Error adding user to group: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
 
