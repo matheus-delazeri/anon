20b4c6ce93d47157400178ef97227b51ae3b9825
Matheus Delazeri <matheusdelazeris@gmail.com>
2024-07-19 10:54:57 -0300

    UI: fix dashboard height

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div
-        class="bg-white m-2 dark:bg-gray-800 border border-gray-100 dark:border-gray-700 overflow-hidden shadow-sm sm:rounded-lg"
-        style="height: 98%">
+        class="bg-white dark:bg-gray-800 border border-gray-100 dark:border-gray-700 overflow-hidden shadow-sm sm:rounded-lg"
+        style="height: 100%">
         <livewire:layout.chat />
     </div>
 </x-app-layout>
